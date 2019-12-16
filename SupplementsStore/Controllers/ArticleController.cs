1428e253addf932d35c59db1f616124064a3e346
Mario Damian <ro_maryo_98@yahoo.com>
2019-12-16 21:07:50 +0200

    Added category and article controllers and models and some authorizations

---
+++ SupplementsStore/Controllers/ArticleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupplementsStore.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ SupplementsStore/Models/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupplementsStore.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Titlul este obligatoriu")]
+        [StringLength(30, ErrorMessage = "Titlul nu poate avea mai mult de 30 de caractere")]
+        public string Title;
+        
+        [Required (ErrorMessage = "Continutul articolului este obligatoriu")]
+        public string Content;
+
+        public int Rating;
+
+        [Required(ErrorMessage = "Categoria este obligatorie")]
+        public int CategoryId { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
